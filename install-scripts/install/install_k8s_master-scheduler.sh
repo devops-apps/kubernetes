cfe1d329d34282c3dd8245a4219aaf8655349b4a
keryy <670706029@qq.com>
2019-06-13 13:13:40 +0800

    update install_k8s_master-scheduler.sh

--- install-scripts/install/install_k8s_master-scheduler.sh
+++ install-scripts/install/install_k8s_master-scheduler.sh
@@ -17,11 +17,14 @@ KUBE_NAME=kube-scheduler
 K8S_INSTALL_PATH=/data/apps/k8s/kubernetes
 K8S_BIN_PATH=${K8S_INSTALL_PATH}/sbin
 K8S_LOG_DIR=${K8S_INSTALL_PATH}/logs
-K8S_KUBECONFIG_PATH=/etc/k8s/kubeconfig
+K8S_CONF_PATH=/etc/k8s/kubernetes
+KUBE_CONFIG_PATH=/etc/k8s/kubeconfig
 CA_DIR=/etc/k8s/ssl
 SOFTWARE=/root/software
 VERSION=v1.14.2
 DOWNLOAD_URL=https://github.com/devops-apps/download/raw/master/kubernetes/kubernetes-server-${VERSION}-linux-amd64.tar.gz
+ETH_INTERFACE=eth1
+LISTEN_IP=$(ifconfig | grep -A 1 ${ETH_INTERFACE} |grep inet |awk '{print $2}')
 USER=k8s
 
 
@@ -45,20 +48,45 @@ else
 fi
 
 if [ ! -d "$K8S_CONF_PATH" ]; then
-     mkdir -p $K8S_KUBECONFIG_PATH
+     mkdir -p $K8S_CONF_PATH
+     chmod 755 $K8S_CONF_PATH
 fi
 
-### 2.Install kube-apiserver binary of kubernetes.
+if [ ! -d "$KUBE_CONFIG_PATH" ]; then
+     mkdir -p $KUBE_CONFIG_PATH
+     chmod 755 $KUBE_CONFIG_PATH
+fi
+
+### 2.Install kube-scheduler binary of kubernetes.
 if [ ! -f "$SOFTWARE/kubernetes-server-${VERSION}-linux-amd64.tar.gz" ]; then
      wget $DOWNLOAD_URL -P $SOFTWARE >>/tmp/install.log  2>&1
 fi
 cd $SOFTWARE && tar -xzf kubernetes-server-${VERSION}-linux-amd64.tar.gz -C ./
 cp -fp kubernetes/server/bin/$KUBE_NAME $K8S_BIN_PATH
-ln -sf  $K8S_BIN_PATH/* /usr/local/bin
+ln -sf  $K8S_BIN_PATH/${KUBE_NAME} /usr/local/bin
 chown -R $USER:$USER $K8S_INSTALL_PATH
 chmod -R 755 $K8S_INSTALL_PATH
 
-### 3.Install the kube-scheduler service.
+### 3.Create configure file of kube-scheduler.
+cat >${K8S_CONF_PATH}kube-scheduler.yaml<<EOF
+apiVersion: kubescheduler.config.k8s.io/v1alpha1
+kind: KubeSchedulerConfiguration
+bindTimeoutSeconds: 600
+clientConnection:
+  burst: 200
+  kubeconfig: "${KUBE_CONFIG_PATH}/${KUBE_NAME}.kubeconfig"
+  qps: 100
+enableContentionProfiling: false
+enableProfiling: true
+hardPodAffinitySymmetricWeight: 1
+healthzBindAddress: ${LISTEN_IP}:10251
+leaderElection:
+  leaderElect: true
+metricsBindAddress: ${LISTEN_IP}:10251
+EOF
+
+
+### 4.Install the kube-scheduler service.
 cat >/usr/lib/systemd/system/${KUBE_NAME}.service<<"EOF"
 [Unit]
 Description=Kubernetes kube-scheduler Service
@@ -69,9 +97,22 @@ After=etcd.service
 [Service]
 User=${USER}
 ExecStart=${K8S_BIN_PATH}/${KUBE_NAME} \
-  --address=127.0.0.1 \
+  --config=${K8S_CONF_PATH}/kube-scheduler.yaml \\
+  --address=${LISTEN_IP} \\
+  --secure-port=10259 \\
+  --port=0 \\
+  --tls-cert-file=/etc/kubernetes/cert/kube-scheduler.pem \\
+  --tls-private-key-file=/etc/kubernetes/cert/kube-scheduler-key.pem \\
   --kubeconfig=${K8S_KUBECONFIG_PATH}/${KUBE_NAME}.kubeconfig \
-  --leader-elect=true \
+  --authentication-kubeconfig=${KUBE_CONFIG_PATH}/${KUBE_NAME}.kubeconfig \\
+  --authorization-kubeconfig=${KUBE_CONFIG_PATH}/${KUBE_NAME}.kubeconfig \\
+  --client-ca-file=${CA_DIR}/ca.pem \\
+  --requestheader-allowed-names="" \\
+  --requestheader-client-ca-file=${CA_DIR}/ca.pem \\
+  --requestheader-extra-headers-prefix="X-Remote-Extra-" \\
+  --requestheader-group-headers=X-Remote-Group \\
+  --requestheader-username-headers=X-Remote-User \\
+  --leader-elect=true \\
   --alsologtostderr=true \
   --logtostderr=false \
   --log-dir=${K8S_LOG_DIR/$KUBE_NAME} \
